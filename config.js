794093f8fa75cf69a8d7a5864fdbb0c598526587
xerocb <ciaransbrady@gmail.com>
2025-04-06 23:06:40 +0100

    Add CORS origin and credentials

--- config.js
+++ config.js
@@ -10,5 +10,7 @@ module.exports = {
         PASSWORD: process.env.DBPASSWORD,
         PORT: process.env.DBPORT
     },
-    SESSION_SECRET: process.env.SESSION_SECRET
+    SESSION_SECRET: process.env.SESSION_SECRET,
+    SALT_ROUNDS: process.env.SALT_ROUNDS,
+    FRONT_END_ORIGIN: process.env.FRONT_END_ORIGIN
 };
--- loaders/express.js
+++ loaders/express.js
@@ -1,10 +1,14 @@
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const session = require('express-session');
+const FRONT_END_ORIGIN = require('../config').FRONT_END_ORIGIN;
 const SESSION_SECRET = require('../config').SESSION_SECRET;
 
 module.exports = (app) => {
-    app.use(cors());
+    app.use(cors({
+        origin: FRONT_END_ORIGIN,
+        credentials: true
+    }));
     app.use(bodyParser.json());
 
     app.use(
