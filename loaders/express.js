7bf3bfd7041c6c123d96958b8a18adb51623fbf5
xerocb <ciaransbrady@gmail.com>
2025-04-07 19:22:16 +0100

    Change cookies to secure

--- loaders/express.js
+++ loaders/express.js
@@ -17,7 +17,7 @@ module.exports = (app) => {
             resave: false,
             saveUninitialized: false,
             cookie: {
-                secure: false,
+                secure: true,
                 maxAge: 24 * 60 * 60 * 1000
             }
         })
