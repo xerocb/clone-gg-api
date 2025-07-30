fc76c3a17210760b675b107046fa7a1be0d89662
xerocb <ciaransbrady@gmail.com>
2025-07-30 16:24:21 +0100

    Add GET / route

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ const loaders = require('./loaders');
 const PORT = require('./config').PORT;
 
 async function startServer() {
+    app.get('/', (req, res) => res.status(200).send('OK'));
+
     loaders(app);
 
     app.listen(PORT, () => {
         console.log(`Server listening on port ${PORT}.`);
-    })
+    });
 }
 
 startServer();
